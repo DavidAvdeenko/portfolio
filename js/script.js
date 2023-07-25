70862f01f0a1589f417ba8476fa0e32ee8262ba0
David <example@gmail.com>
2023-07-25 17:12:40 +0300

    filled lorems, added and animations and fixed script.js file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector('.hamburger'),
    menu = document.querySelector('.menu'),
-   closeElem = document.querySelector('.menu__close');
+   closeElem = document.querySelector('.menu__close'),
+   itemClose = document.querySelector('.menu__list');
+
 
 hamburger.addEventListener('click', () => {
    menu.classList.add('active');
 });
 
+itemClose.addEventListener('click', () => {
+   menu.classList.remove('active');
+});
 closeElem.addEventListener('click', () => {
    menu.classList.remove('active');
 });
@@ -17,3 +22,18 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach((item, i) => {
    lines[i].style.width = item.innerHTML;
 });
+
+$(window).scroll(function () {
+   if ($(this).scrollTop() > 1000) {
+      $('.pageup').fadeIn();
+   } else {
+      $('.pageup').fadeOut();
+   }
+
+});
+
+
+$(document).ready(function () {
+   new WOW().init();
+});
+
